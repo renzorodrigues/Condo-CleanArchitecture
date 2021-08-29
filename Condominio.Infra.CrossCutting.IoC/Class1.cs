8513dfeddb9e0e1a72d3781a277736eb24efa14d
renzorodrigues <renzors@gmail.com>
2021-08-29 16:45:15 -0300

    acrescentando projeto crosscutting ioc

---
+++ Condominio.Infra.CrossCutting.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Condominio.Infra.CrossCutting.IoC
+{
+    public class Class1
+    {
+    }
+}
---
+++ Condominio.Infra.CrossCutting.IoC/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Condominio.Infra.CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection Teste(IServiceCollection service, IConfiguration configuration)
+        {
+            return service;
+        }
+    }
+}
